508de4120d8519ba51918a90218f45a6bf57d82f
adebare <adebaresulaimon04@gmail.com>
2022-04-18 22:39:31 +0100

    updated file for _printf function

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+/**
+ * struct types_arguments - Struct op
+ *
+ * @t: The operator
+ * @f: The function associated
+ */
+typedef struct types_arguments
+{
+	char t;
+	int (*f)(va_list va);
+} types;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_number(va_list va);
+
+#endif
